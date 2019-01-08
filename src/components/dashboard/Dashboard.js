4a46daf2af32e058f54bd8401d469cd9bff87ce1
SathishSaminathan <Sathishcse1975@gmail.com>
2019-01-08 14:21:03 +0530

    initial_commit

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col m6 s12">Dashboard</div>
+        <div className="col m6 s12">Dashboard</div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
